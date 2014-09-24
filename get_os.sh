eac9d46e37be70ab7e8b3bdbc0072840482f6eda
kv <devnull@localhost>
2014-09-24 16:33:35 +0700

    Added a script to detect OS

---
+++ get_os.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$(uname)" == "Darwin" ]; then
+	echo "darwin"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+	echo "linux"
+elif [ "$(expr substr $(uname -s) 1 6)" == "CYGWIN" ]; then
+	echo "cygwin"
+fi
