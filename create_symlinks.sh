fdf31a447ba9979717f3f28394ec0bb80a538874
vk <vk@localhost>
2015-09-02 23:10:10 +0600

    dotfiles: use GNU Stow to manage the installation of dotfiles instead of     a self-made script

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -3,28 +3,22 @@
 # Thanks to SO: http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
 wd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 os=$($wd/get_os.sh)
-# @TODO: read '--dry-run' parameter
-# cmd="echo "
-cmd=""
 
-# The default behavior of ln if the second arg is a directory
-# is to place a link to the first arg inside it
-$cmd ln -s $wd/.ncftp ~
-$cmd ln -s $wd/.vim ~
-
-$cmd ln -s $wd/.ackrc ~/.ackrc
-$cmd ln -s $wd/.bash_profile ~/.bash_profile
-$cmd ln -s $wd/.bashrc ~/.bashrc
+stow ack
+stow archey3
+stow bash
+stow bspwm
+stow git
+stow mercurial
 if [ "$os" == "cygwin" ]; then
-	$cmd ln -s $wd/.minttyrc ~/.minttyrc
+    stow mintty
+    stow ncftp
 fi
-$cmd ln -s $wd/.profile ~/.profile
-$cmd ln -s $wd/.tmux.conf ~/.tmux.conf
-$cmd ln -s $wd/.vim/.vimrc ~/.vimrc
-$cmd ln -s $wd/.zpreztorc ~/.zpreztorc
-$cmd ln -s $wd/.zshrc ~/.zshrc
-$cmd ln -s $wd/.zshenv ~/.zshenv
-
-mkdir ~/.subversion
-rm -rf ~/.subversion/config
-$cmd ln -s $wd/svn_config ~/.subversion/config
+stow pentadactyl
+stow svn
+stow termite
+stow tmux
+stow vim
+stow vimpager
+stow x
+stow zsh
