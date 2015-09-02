8335fb18c872ba376184b62109762fbdcadd3a43
vk <vk@localhost>
2015-09-02 23:08:26 +0600

    dotfiles: use sh instead of bash in create_symlinks script

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Thanks to SO: http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
 wd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
