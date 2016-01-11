20c30f3266a5f7dd0001e2da3c141ba5cbf66761
vk <vk@localhost>
2016-01-11 15:10:00 -0500

    dotfiles: don't install some stuff under Linux

--- install.sh
+++ install.sh
@@ -4,32 +4,37 @@
 wd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 os=$($wd/get-os.sh)
 
-stow ack
 stow aria2
-stow archey3
-stow bash
 stow bspwm
 stow git
 stow goobook
-stow mercurial
 stow mps-youtube
 stow mpv
-if [ "$os" == "cygwin" ]; then
-    stow mintty
-    stow ncftp
-fi
-stow pentadactyl
 stow qutebrowser
-stow svn
 stow termite
 stow tmux
 stow tmuxinator
-stow vim
 # @TODO: create vim's undo and backups directories
+stow vim
 stow vimpager
 stow x
 stow youtube-dl
 stow zathura
 stow zsh
+
+stow bash
+stow mercurial
+stow svn
+
+if [[ "$os" == "cygwin" ]]; then
+    stow ack
+    stow mintty
+    stow ncftp
+    stow pentadactyl
+fi
+
+if [[ -e /etc/arch-release ]]; then
+    stow archey3
+fi
 
 ./install-vim-neobundle.sh
